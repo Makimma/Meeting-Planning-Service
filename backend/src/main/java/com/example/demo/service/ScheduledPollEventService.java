42a273c0393560fab5181c5dafd44d53eafeb347
Pham Trung Hoang <chfam@edu.hse.ru>
2024-08-23 03:50:52 +0300

    Delete unnecessary services

--- backend/src/main/java/com/example/demo/service/ScheduledPollEventService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-public interface ScheduledPollEventService {
-//    void schedulePollEvent(CreateMeetingFromPollDTO createMeetingFromPollDTO);
-//    void cancelPollEvent(CreateMeetingFromPollDTO createMeetingFromPollDTO);
-//    void updatePollEvent(CreateMeetingFromPollDTO createMeetingFromPollDTO);
-}
